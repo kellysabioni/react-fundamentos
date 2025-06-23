da27b31496bfcc62712f1e9ce7153e9c97353bdc
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-06-23 09:35:27 -0300

    Atualiza ZERANDO main.tsx
    Comentários

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
+// Ajuda detectar uso de recursos obsoletos (APIs, funções, padrões etc)
 import { StrictMode } from 'react'
+// Usado para criar a 'raíz' da aplicação React a partir de um elemento HTML
 import { createRoot } from 'react-dom/client'
-import './index.css'
+// Importa o componente principal da aplicação 
+// O componente App é o ponto de entrada da aplicação React
 import App from './App.tsx'
 
+// Seleciona o elemento HTML root, cria a raíz da aplicação e renderiza o componente App. 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
