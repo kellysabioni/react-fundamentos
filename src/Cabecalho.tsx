8ed60a8ee8e08444b5d9d45647e0a733520e56cc
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-06-23 11:14:41 -0300

    Atualiza componente com importação de imagem

--- src/Cabecalho.tsx
+++ src/Cabecalho.tsx
@@ -1,7 +1,10 @@
+/* Tudo o que for uasdo dentro de componentes (imagens, fontes etc), deve ser importado. Desta forma, no momento do build, haverá otimizações para este conteúdo. */
 export default function Cabecalho() {
   return (
     <header>
-      <h1>😱 Olá, React! 😱</h1>
+      <h1>
+        <img src={logo} alt="" /> 😱 Olá, React! 😱
+      </h1>
       <hr />
       <nav>
         <a href="">Home</a>
