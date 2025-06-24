a89e3c87f2848728c355d0058382dee5c3064dc4
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-06-24 09:45:36 -0300

    Atualiza com seletor classe

--- src/componentes/Footer.tsx
+++ src/componentes/Footer.tsx
@@ -1,10 +1,14 @@
 export default function Footer() {
   return (
-    <footer>
-      <h2>Pequeno site criado com React </h2>
-      <p>
-        Desenvolvido por <b>Kelly Sabioni</b> &copy; 2025{" "}
-      </p>
-    </footer>
+    <>
+      <footer>
+        <h2>Pequeno site criado com React </h2>
+        <p>
+          Desenvolvido por <b>Kelly Sabioni</b> &copy; 2025{" "}
+        </p>
+      </footer>
+      <hr />
+      <p className="outro-texto" >Qualquer coisa...</p>
+    </>
   );
 }
