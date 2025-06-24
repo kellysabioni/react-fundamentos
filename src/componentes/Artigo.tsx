9f3c62578e058495222f1e24e0b4ab9b91aebf72
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-06-24 09:14:54 -0300

    Cria componente Artigo

---
+++ src/componentes/Artigo.tsx
@@ -0,0 +1,12 @@
+export default function Artigo() {
+  return (
+    <article>
+      <h3>Artigo 1 de exemplo </h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati,
+        quasi eligendi! Corrupti nesciunt quo officiis, distinctio perferendis
+        earum quam unde. Iusto vero distinctio perferendis deleniti?
+      </p>
+    </article>
+  );
+}
--- src/componentes/Conteudo.tsx
+++ src/componentes/Conteudo.tsx
@@ -1,3 +1,5 @@
+import Artigo from "./Artigo";
+
 export default function Conteudo() {
   return (
     <main>
@@ -5,31 +7,12 @@ export default function Conteudo() {
         <h2>Seja bem-vindo(a) aos Fundamentos do React!</h2>
         <p>Este é um exemplo de aplicação React.</p>
 
-        <article>
-          <h3>Artigo 1 de exemplo </h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati,
-            quasi eligendi! Corrupti nesciunt quo officiis, distinctio
-            perferendis earum quam unde. Iusto vero distinctio perferendis
-            deleniti?
-          </p>
+        <Artigo />
 
-          <h3>Artigo 2 de exemplo </h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati,
-            quasi eligendi! Corrupti nesciunt quo officiis, distinctio
-            perferendis earum quam unde. Iusto vero distinctio perferendis
-            deleniti?
-          </p>
-
-          <h3>Artigo 3 de exemplo </h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati,
-            quasi eligendi! Corrupti nesciunt quo officiis, distinctio
-            perferendis earum quam unde. Iusto vero distinctio perferendis
-            deleniti?
-          </p>
-        </article>
+        <Artigo />
+        
+        <Artigo />
+        
       </section>
     </main>
   );
