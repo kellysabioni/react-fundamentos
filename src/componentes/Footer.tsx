4ddf8e2264729f332cc6abf4f49aff766e9bc51a
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-06-24 09:55:15 -0300

    Cria com css in js

--- src/componentes/Footer.tsx
+++ src/componentes/Footer.tsx
@@ -3,12 +3,12 @@ export default function Footer() {
     <>
       <footer>
         <h2>Pequeno site criado com React </h2>
-        <p>
+        <p style={({ color: "red" , fontWeight: "bold" })}>
           Desenvolvido por <b>Kelly Sabioni</b> &copy; 2025{" "}
         </p>
       </footer>
       <hr />
-      <p className="outro-texto" >Qualquer coisa...</p>
+      <p className="outro-texto">Qualquer coisa...</p>
     </>
   );
 }
