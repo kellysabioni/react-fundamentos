f0115b2353b106439368a2c581f1878e0194c1a3
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-06-25 11:51:56 -0300

    Adiciona AvisoImportante componente
    Apresenta o componente AvisoImportante com conteúdo de advertência estilizado e o integra ao layout do Conteudo. A nova seção destaca informações importantes para alunos em risco de reprovação, usando caixas visualmente distintas.

--- src/componentes/Conteudo/AvisoImportante/AvisoImportante.tsx
+++ src/componentes/Conteudo/AvisoImportante/AvisoImportante.tsx
@@ -1,3 +1,14 @@
 export default function AvisoImportante() {
-  return <div>AvisoImportante</div>;
+  return (
+    <section className="bg-yellow-200 br- border-l-4 border-yellow-500 p-4 mt-2  ">
+      <h3 className=" font-bold text-2xl text-orange-600 " >Aviso importante:</h3>
+      <p>Alguns alunos e alunas estão perigando reprovação nesta UC!</p>
+    
+    <div className="flex justify-around  " >
+      <div className="bg-blue-300 p-8 m-2 rounded-xl " >UM</div>
+      <div className="bg-green-300 p-8 m-2 rounded-xl" >DOIS</div>
+      <div className="bg-red-300 p-8 m-2 rounded-xl" >TRES</div>
+    </div>
+    </section>
+  );
 }
--- src/componentes/Conteudo/Conteudo.tsx
+++ src/componentes/Conteudo/Conteudo.tsx
@@ -1,12 +1,15 @@
 import Artigo from "./Artigo/Artigo";
 import DicaDoDia from "./DicaDoDia/DicaDoDia";
 import styles from "./Conteudo.module.css";
+import AvisoImportante from "./AvisoImportante/AvisoImportante";
 
 export default function Conteudo() {
   return (
     <main className={styles.conteudo}>
       {/* Para combinar classes, use template string */}
-      <section className={`${styles.principal} ${styles.arredondada} ${styles.sombra}`}>
+      <section
+        className={`${styles.principal} ${styles.arredondada} ${styles.sombra}`}
+      >
         <h2>Seja bem-vindo(a) aos Fundamentos do React!</h2>
         <p>Este é um exemplo de aplicação React.</p>
 
@@ -15,10 +18,11 @@ export default function Conteudo() {
         <Artigo />
 
         <Artigo />
-
       </section>
 
       <DicaDoDia />
+
+      <AvisoImportante />
     </main>
   );
 }
