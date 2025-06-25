9c9c573d9bb38277b973dd107e8357767b8d67de
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-06-25 11:17:55 -0300

    Organiza pastas dentro de conteudo

--- src/componentes/Conteudo/Conteudo.tsx
+++ src/componentes/Conteudo/Conteudo.tsx
@@ -1,5 +1,5 @@
-import Artigo from "../Artigo/Artigo";
-import DicaDoDia from "../DicaDoDia/DicaDoDia";
+import Artigo from "./Artigo/Artigo";
+import DicaDoDia from "./DicaDoDia/DicaDoDia";
 import styles from "./Conteudo.module.css";
 
 export default function Conteudo() {
