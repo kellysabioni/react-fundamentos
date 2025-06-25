39940cbe7fd9e03366a5ad2f807a9a330eb0bc26
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-06-25 09:23:15 -0300

    Atualização estilos do conteudo através de modulos

--- src/componentes/Conteudo/Conteudo.tsx
+++ src/componentes/Conteudo/Conteudo.tsx
@@ -1,18 +1,19 @@
 import Artigo from "../Artigo/Artigo";
+import styles from "./Conteudo.module.css";
 
 export default function Conteudo() {
   return (
-    <main>
-      <section>
+    <main className={styles.conteudo}>
+      {/* Para combinar classes, use template string */}
+      <section className={`${styles.arredondada} ${styles.sombra}`}>
         <h2>Seja bem-vindo(a) aos Fundamentos do React!</h2>
         <p>Este é um exemplo de aplicação React.</p>
 
         <Artigo />
 
         <Artigo />
-        
+
         <Artigo />
-        
       </section>
     </main>
   );
