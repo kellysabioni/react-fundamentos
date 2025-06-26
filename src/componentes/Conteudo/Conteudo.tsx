654a00d4ee83dd86d7cd738d70f5ddc4252628e4
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-06-26 09:11:40 -0300

    Torna 'nome' props opcional e atribui valor padrão na chamada

--- src/componentes/Conteudo/Conteudo.tsx
+++ src/componentes/Conteudo/Conteudo.tsx
@@ -12,7 +12,7 @@ export default function Conteudo() {
         className={`${styles.principal} ${styles.arredondada} ${styles.sombra}`}
       >
         <h2 className="font-bold text-lg ">Fundamentos do React!</h2>
-        <Saudacao nome="Kelly" classe="bg-emerald-100" />
+        <Saudacao classe="bg-emerald-100" />
         <Saudacao nome="Fulano" classe="bg-red-100" />
 
         <p>Este é um exemplo de aplicação React.</p>
--- src/componentes/Conteudo/Saudacao.tsx
+++ src/componentes/Conteudo/Saudacao.tsx
@@ -1,9 +1,12 @@
-type Props = {
-  nome: string;
+type SaudacaoProps = {
+  nome?: string;
   classe: string;
 };
 
-export default function Saudacao({nome, classe}: Props) {
+export default function Saudacao({
+  nome = "Visitante",
+  classe,
+}: SaudacaoProps) {
   return (
     <p className={`my-2 rounded text-center ${classe} `}>
       Seja bem-vindo(a) <b>{nome}</b>!
