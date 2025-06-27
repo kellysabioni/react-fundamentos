f9e65c2fbb13eea3febca2eb615e119dcf538fcd
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-06-27 09:05:17 -0300

    Adiciona evento in line

--- src/componentes/Cabecalho/Cabecalho.tsx
+++ src/componentes/Cabecalho/Cabecalho.tsx
@@ -4,9 +4,9 @@ import estilos from "./Cabecalho.module.css";
 
 export default function Cabecalho() {
   return (
-    <header className={ estilos.topoSite } >
-      <h1 className={estilos.titulo} >
-        <img className={estilos.logo} src={logo} alt="" />  Olá, React! 
+    <header className={estilos.topoSite}>
+      <h1 onClick={() => alert("Olá!")} className={estilos.titulo}>
+        <img className={estilos.logo} src={logo} alt="" /> Olá, React!
       </h1>
       <hr />
 
