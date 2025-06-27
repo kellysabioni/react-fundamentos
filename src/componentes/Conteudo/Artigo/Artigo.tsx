ecf7868224c55558675b2420bb1972627069d70f
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-06-27 10:54:00 -0300

    Cria evento para selecionar artigo e mudar bg color

--- src/componentes/Conteudo/Artigo/Artigo.tsx
+++ src/componentes/Conteudo/Artigo/Artigo.tsx
@@ -1,17 +1,34 @@
 import styles from "./Artigo.module.css";
 import type { Curso } from "../../../types/Curso";
+import { useState } from "react";
 
 type ArtigoProps = {
   dados: Curso;
 };
 
 export default function Artigo({ dados }: ArtigoProps) {
+  const [selecionado, setSelecionado] = useState(false);
+
+  const selecionaArtigo = () => {
+    setSelecionado((selecionado) => !selecionado);
+  };
+
   const { titulo, preco, categoria } = dados;
   return (
-    <article className={styles.artigo}>
+    <article
+      onClick={selecionaArtigo}
+      className={styles.artigo}
+      style={{
+        backgroundColor: selecionado ? "#e0f7fa" : "#fff", 
+      }}
+    >
       <h3> {titulo} </h3>
-      <p><b>Categoria:</b> {categoria}</p>
-      <p><b>Preço:</b> {preco}</p>
+      <p>
+        <b>Categoria:</b> {categoria}
+      </p>
+      <p>
+        <b>Preço:</b> {preco}
+      </p>
     </article>
   );
 }
