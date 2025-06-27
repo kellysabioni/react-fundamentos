e7ad4b75a921862fae14ca06feedc8a67037355d
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-06-27 11:27:12 -0300

    Cria botões para filtro de categoria

--- src/componentes/Conteudo/Conteudo.tsx
+++ src/componentes/Conteudo/Conteudo.tsx
@@ -3,8 +3,12 @@ import styles from "./Conteudo.module.css";
 import AvisoImportante from "./AvisoImportante/AvisoImportante";
 import Saudacao from "./Saudacao";
 import ListaCursos from "./ListaCursos/ListaCursos";
+import cursos from "../../data/cursos";
 
 export default function Conteudo() {
+
+  const categorias = [...new Set(cursos.map(curso => curso.categoria))];
+
   return (
     <main className={styles.conteudo}>
       <section
@@ -14,6 +18,12 @@ export default function Conteudo() {
         <Saudacao nome="Kelly" classe="bg-blue-100" />
 
         <p>Este é um exemplo de aplicação React.</p>
+
+        {
+        categorias.map((categoria) =>{
+          return <button className="rounded py-1 px-2 mr-2 m-3 bg-blue-200 hover:bg-blue-500 " >{categoria}</button>})
+        
+        }
 
         <ListaCursos/>
 
