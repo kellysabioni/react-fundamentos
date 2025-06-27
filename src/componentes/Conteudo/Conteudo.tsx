c1bf144f9167b0720ac158e0abfa24b7a11e8eb3
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-06-27 11:38:12 -0300

    Adiciona filtro para state categoria
    Introduces a state variable to track the selected category and updates the UI to display the selected category when a button is clicked. This enhances interactivity by allowing users to see which category is currently active.

--- src/componentes/Conteudo/Conteudo.tsx
+++ src/componentes/Conteudo/Conteudo.tsx
@@ -4,10 +4,13 @@ import AvisoImportante from "./AvisoImportante/AvisoImportante";
 import Saudacao from "./Saudacao";
 import ListaCursos from "./ListaCursos/ListaCursos";
 import cursos from "../../data/cursos";
+import { useState } from "react";
 
 export default function Conteudo() {
+  /* Definindo o state para categoria ativa/selecionada podendo ser null (valor padrão/inicial) ou string (quando uma categoria for selecionada) */
+  const [categoriaAtiva, setCategoriaAtiva] = useState<null | string>(null);
 
-  const categorias = [...new Set(cursos.map(curso => curso.categoria))];
+  const categorias = [...new Set(cursos.map((curso) => curso.categoria))];
 
   return (
     <main className={styles.conteudo}>
@@ -19,14 +22,21 @@ export default function Conteudo() {
 
         <p>Este é um exemplo de aplicação React.</p>
 
-        {
-        categorias.map((categoria) =>{
-          return <button className="rounded py-1 px-2 mr-2 m-3 bg-blue-200 hover:bg-blue-500 " >{categoria}</button>})
-        
-        }
+        {categorias.map((categoria) => {
+          return (
+            <button onClick={() => setCategoriaAtiva(categoria)} className="rounded py-1 px-2 mr-2 m-3 bg-blue-200 hover:bg-blue-500 ">
+              {categoria}
+            </button>
+          );
+        })}
 
-        <ListaCursos/>
+        {categoriaAtiva && (
+          <p>
+            Categoria selecionada: <b>{categoriaAtiva}</b>
+          </p>
+        )}
 
+        <ListaCursos />
       </section>
 
       <DicaDoDia />
