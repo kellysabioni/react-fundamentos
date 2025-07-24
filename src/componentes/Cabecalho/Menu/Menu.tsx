9ec5d70f4a2722cf9b3567f48a9f285189de0075
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-07-24 10:47:44 -0300

    Atualiza com tag LINK do react

--- src/componentes/Cabecalho/Menu/Menu.tsx
+++ src/componentes/Cabecalho/Menu/Menu.tsx
@@ -1,11 +1,12 @@
+import { Link } from "react-router-dom";
 import styles from "./Menu.module.css";
 
 export default function Menu() {
   return (
-    <nav className={ styles.menu } >
-      <a href="">Home</a>
-      <a href="">Sobre</a>
-      <a href="">Contato</a>
+    <nav className={styles.menu}>
+      <Link to="/">Home</Link>
+      <Link to="/sobre">Sobre</Link>
+      <Link to="/contato">Contato</Link>
     </nav>
   );
 }
