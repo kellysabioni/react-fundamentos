76cae778141ed833bd2f6a25d6424c72f309ed9d
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-07-24 11:58:10 -0300

    Atualiza pagina 404

--- src/componentes/Pagina404/Pagina404.tsx
+++ src/componentes/Pagina404/Pagina404.tsx
@@ -1,8 +1,16 @@
+import { Link } from "react-router-dom";
+import imagem from "../../Assets/Pagina404.png";
+
 export default function Pagina404() {
   return (
     <div>
-       
+      <h1>
+        <img src={imagem} alt="Ops! Página não encontrada" />
+      </h1>
 
+      <p  >
+        <Link to="/"> Página Inicial </Link>
+      </p>
     </div>
-  )
+  );
 }
