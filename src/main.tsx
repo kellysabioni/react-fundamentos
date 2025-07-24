899bc8818bf1552babc9f1fc5d203fdd9696a6b4
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-07-24 09:58:13 -0300

    Importa BrowserRouter e atualiza chamada de App

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-// Importando a folha de estilos CSS global
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
 import "./global.css";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
+/* O BrowserRouter deve envolver toda a aplicação, habilitando assim a navagação por rotas. */
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
-
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
