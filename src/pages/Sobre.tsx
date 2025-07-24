b2bb92d1e77bdb7c9cd5a3d16c6e565588ed1fc1
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-07-24 09:52:07 -0300

    Cria Sobre

---
+++ src/pages/Sobre.tsx
@@ -0,0 +1,11 @@
+export default function Sobre() {
+  return (
+    <section>
+        <h2>Sobre nossa empresa </h2>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora non modi, doloribus sed distinctio vero iste numquam ab explicabo earum, odio unde quasi, voluptatum dignissimos.
+        </p>
+    </section>
+
+  )
+}
