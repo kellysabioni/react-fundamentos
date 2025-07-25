f3fca37e9ffe4879a2b752ebed0df65246ed99ac
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-07-25 09:44:14 -0300

    Atualiza estilos Pagina404

--- src/componentes/Pagina404/Pagina404.tsx
+++ src/componentes/Pagina404/Pagina404.tsx
@@ -1,14 +1,19 @@
+//import styles from "Pagina404.module.css";
 import { Link } from "react-router-dom";
 import imagem from "../../Assets/Pagina404.png";
+import { useEffect } from "react";
 
 export default function Pagina404() {
+  useEffect(() => {
+    document.title = "Ops! Essa página não existe!";
+  });
   return (
     <div>
       <h1>
         <img src={imagem} alt="Ops! Página não encontrada" />
       </h1>
 
-      <p  >
+      <p>
         <Link to="/"> Página Inicial </Link>
       </p>
     </div>
