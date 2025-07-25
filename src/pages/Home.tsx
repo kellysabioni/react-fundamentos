ab0ae834325994acf9cb409d088ced302e878047
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-07-25 09:23:15 -0300

    Atualiza com useEffect

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,10 @@
+import { useEffect } from "react";
 import Conteudo from "../componentes/Conteudo/Conteudo";
 
 export default function Home() {
+  useEffect(() => {
+    console.log("Componente Montado/Renderizado");
+  }, []);
+
   return <Conteudo />;
 }
