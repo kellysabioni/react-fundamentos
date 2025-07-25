d5232d41ce369fa6462c1ec8772452e6bb0311ce
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-07-25 09:39:38 -0300

    Atualiza useEffect

--- src/pages/Sobre.tsx
+++ src/pages/Sobre.tsx
@@ -1,12 +1,17 @@
+import { useEffect } from "react";
+
 export default function Sobre() {
-  
+  useEffect(() => {
+    document.title = "Sobre nós";
+  });
   return (
     <section>
-        <h2>Sobre nossa empresa </h2>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora non modi, doloribus sed distinctio vero iste numquam ab explicabo earum, odio unde quasi, voluptatum dignissimos.
-        </p>
+      <h2>Sobre nossa empresa </h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora non
+        modi, doloribus sed distinctio vero iste numquam ab explicabo earum,
+        odio unde quasi, voluptatum dignissimos.
+      </p>
     </section>
-
-  )
+  );
 }
